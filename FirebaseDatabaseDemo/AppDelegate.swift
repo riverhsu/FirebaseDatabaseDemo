80de85c7960a91681f240893a5edd5ad1ce81164
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-26 17:03:02 +0800

    Step 1: Initialize the Firebase Database Step 2: create a pod file for Firebase database framework Step 3: create a simple GUI for database App

--- FirebaseDatabaseDemo/AppDelegate.swift
+++ FirebaseDatabaseDemo/AppDelegate.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    //Customize initializer for Firebase Database
+    override init(){
+        //configure Firebase database
+        FIRApp.configure()
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- FirebaseDatabaseDemo/ViewController.swift
+++ FirebaseDatabaseDemo/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    //Connect to Firebase database reference
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
