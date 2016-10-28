49507f92e4fd9a59566d5e739b3d6316d225598d
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-28 16:01:13 +0800

    step 4: download the configuration file and input to the project step 5: modify program for synchronizing data with Firebase database step 6: need to change authorization for this testing

--- FirebaseDatabaseDemo/ViewController.swift
+++ FirebaseDatabaseDemo/ViewController.swift
@@ -11,18 +11,44 @@ import Firebase
 
 class ViewController: UIViewController {
 
-    //Connect to Firebase database reference
+    //Connect to real time Firebase database reference
+    // real time database is JSON document tree
+    //let rootRef = FIRDatabase.database().reference()
+    
+    //Sample JSON structure :
+    // Root/condition/sunny
+    // Root/condition/foggy
+    // Now listen to or observe database changes
+    //let conditionRef = rootRef.child("condition")
+    
+    let conditionRef = FIRDatabase.database().reference().child("condition")
+    
+    @IBOutlet weak var conditionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+
+        //conditionRef.observeEventType(eventType: FIRDataEventType, withBlock: (FIRDataSnapshot) -> Void)
+        conditionRef.observe(FIRDataEventType.value){ (snap: FIRDataSnapshot) in
+            self.conditionLabel.text = snap.value as? String
+                            
+        }
     }
 
-
+    @IBAction func sunnyDidTouch(_ sender: AnyObject) {
+        //因為JSON是目錄結構，所以 conditionRef 是指向 condition，設定它的值
+        conditionRef.setValue("Sunny")
+    }
+    
+    @IBAction func foggyDidTouch(_ sender: AnyObject) {
+        conditionRef.setValue("Foggy")
+    }
+    
 }
 
